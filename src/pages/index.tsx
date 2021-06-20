329e7b8fd87d5d52e7be5a07421ea588b7771d49
Riday ♡ <ridayshah2001@gmail.com>
2021-06-20 18:40:15 +0530

    ✨ Feat: basic design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,36 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
+import { text } from '../theme';
 
-const IndexPage = () => <Layout page='home'>Home Page</Layout>;
+const IndexPage = () => (
+	<Layout page='home'>
+		<div style={{ display: 'grid', height: '80vh' }}>
+			<StaticImage
+				style={{
+					gridArea: '1/1',
+					filter: 'brightness(70%)'
+				}}
+				layout='fullWidth'
+				aspectRatio={3 / 1}
+				alt=''
+				src='../images/books.png'
+				formats={['auto', 'webp', 'avif']}
+			/>
+			<div
+				style={{
+					gridArea: '1/1',
+					position: 'relative',
+					placeItems: 'center',
+					display: 'grid',
+					color: text.high
+				}}
+			>
+				<h1>Logo</h1>
+				<button className='btn btn-primary'>Book A Demo</button>
+			</div>
+		</div>
+	</Layout>
+);
 
 export default IndexPage;
