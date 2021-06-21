fe19a15b6a253424918b6d4f949fffc978f7be15
Riday ♡ <ridayshah2001@gmail.com>
2021-06-21 18:59:18 +0530

    🎨 Feat: home page first screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,46 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 import Layout from '../components/layout';
-import { StaticImage } from 'gatsby-plugin-image';
+import SEO from '../components/SEO';
 import { text } from '../theme';
 
 const IndexPage = () => (
 	<Layout page='home'>
-		<div style={{ display: 'grid', height: '80vh' }}>
+		<SEO title='Home' description='The home page.' />
+		<div style={{ display: 'grid', height: '85vh' }}>
 			<StaticImage
 				style={{
 					gridArea: '1/1',
-					filter: 'brightness(70%)'
+					filter: 'brightness(60%)'
 				}}
 				layout='fullWidth'
 				aspectRatio={3 / 1}
 				alt=''
 				src='../images/books.png'
-				formats={['auto', 'webp', 'avif']}
+				formats={['auto', 'webp']}
+				placeholder='dominantColor'
 			/>
 			<div
 				style={{
 					gridArea: '1/1',
 					position: 'relative',
 					placeItems: 'center',
-					display: 'grid',
 					color: text.high
 				}}
+				className='row'
 			>
-				<h1>Logo</h1>
-				<button className='btn btn-primary'>Book A Demo</button>
+				<div className='col-md-6 mx-auto d-flex justify-content-evenly'>
+					<h1>Logo</h1>
+				</div>
+				<div className='row'>
+					<div className='col-9 col-md-4 mx-auto my-4 d-flex justify-content-evenly'>
+						<button className='btn btn-primary btn-lg'>Book A Demo</button>
+					</div>
+					<div className='row'></div>
+					<div className='col-9 col-md-4 mx-auto my-4 d-flex justify-content-evenly'>
+						<button className='btn btn-secondary btn-lg'>Teach with Us</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	</Layout>
