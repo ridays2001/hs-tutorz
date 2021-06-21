4eaf1782cdb247313e60222b605b28dd624371e8
Riday ♡ <ridayshah2001@gmail.com>
2021-06-21 19:00:00 +0530

    🔨 Feat: added plugins for manifest and fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
+const desc = 'Online Economics tutorials for high school and college level.';
+
 module.exports = {
 	siteMetadata: {
-		title: 'HS Tutorialz'
+		title: 'HS Tutorialz',
+		description: desc,
+		// Change this URL later!
+		siteUrl: 'http://192.168.29.xx'
 	},
 	plugins: [
 		'gatsby-plugin-styled-components',
@@ -17,6 +22,30 @@ module.exports = {
 				path: './src/images/'
 			},
 			__key: 'images'
+		},
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				name: 'HS Tutorialz',
+				short_name: 'HST',
+				description: desc,
+				start_url: '/',
+				background_color: '#79352B',
+				theme_color: '#79352B',
+				icon: 'src/images/icon.png',
+				include_favicon: true,
+				cache_busting_mode: 'query',
+				crossOrigin: `use-credentials`,
+				display: 'minimal-ui'
+			}
+		},
+		{
+			resolve: 'gatsby-plugin-web-font-loader',
+			options: {
+				google: {
+					families: ['Playfair Display', 'Playfair Display SC']
+				}
+			}
 		}
 	]
 };
