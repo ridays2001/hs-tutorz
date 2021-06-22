2673c73e49dcbb641fc666cf8bf66ec932e1494c
Riday ♡ <ridayshah2001@gmail.com>
2021-06-22 13:18:40 +0530

    ✨ Feat: careers page

---
+++ src/pages/careers.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+
+const CareersPage = () => (
+	<Layout page='careers'>
+		<SEO title='Teach With Us' description='Teacher registration with HS Tutorialz.' />
+		Careers Page
+	</Layout>
+);
+
+export default CareersPage;
