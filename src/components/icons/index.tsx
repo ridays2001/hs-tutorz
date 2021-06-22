86fa87249fce2df1d2035ff9554865b402e1de63
Riday ♡ <ridayshah2001@gmail.com>
2021-06-22 13:19:26 +0530

    🍱 Chore: added icons

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,5 @@
+export { default as ArrowDownIcon } from './ArrowDown';
+export { default as BookingIcon } from './Booking';
+export { default as ContactIcon } from './Contact';
+export { default as LearningIcon } from './Learning';
+export { default as SelectIcon } from './Select';
