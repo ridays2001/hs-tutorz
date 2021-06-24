dedca903192b2a0d4ab981ada6ea1bc98b7da0e4
Riday ♡ <ridayshah2001@gmail.com>
2021-06-24 19:34:05 +0530

    💄 Style: changed background color

--- src/styles/TutOption.ts
+++ src/styles/TutOption.ts
@@ -1,13 +1,12 @@
 import styled from 'styled-components';
-import { devices, shadows } from '../theme';
+import { colors, devices, shadows } from '../theme';
 
 const TutOption = styled.div`
 	border-radius: 21px;
 	box-shadow: ${shadows.layer1};
 	padding: 2.5em 1em;
 	margin-top: 1.25em;
-	border: 2px solid #bd8e45;
-	background-color: #fbbd5c;
+	border: 2px solid ${colors.dark};
 
 	${devices.md} {
 		margin-top: 0;
