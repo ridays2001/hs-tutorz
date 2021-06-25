945c4ecedab6975352df00960d72dc83f8b9bf07
Riday ♡ <ridayshah2001@gmail.com>
2021-06-25 17:54:32 +0530

    ✨ Feat: testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,10 +1,23 @@
 import React, { Fragment } from 'react';
-import {} from 'swiper/react';
+import { Helmet } from 'react-helmet';
+import Slider from 'react-slick';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	margin-bottom: 10vh;
+	cursor: grab;
+
+	&:active {
+		cursor: grabbing;
+	}
+`;
 
 interface Testimonial {
 	name: string;
 	review: string;
 }
+
+// cSpell: disable
 const testimonials: Array<Testimonial> = [
 	{
 		name: 'Test',
@@ -67,17 +80,42 @@ const testimonials: Array<Testimonial> = [
 			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
 	}
 ];
+// cSpell: enable
 
 const Testimonials = () => (
 	<Fragment>
-		{/* <div className='container'>
-			{testimonials.map(({ name, review }, i) => (
-				<div key={i}>
-					By {name}
-					{review}
-				</div>
-			))}
-		</div> */}
+		<Helmet>
+			<link
+				rel='stylesheet'
+				type='text/css'
+				href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css'
+			/>
+			<link
+				rel='stylesheet'
+				type='text/css'
+				href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css'
+			/>
+		</Helmet>
+		<Container className='col-11 col-md-9 col-lg-8 col-xl-6 mx-auto'>
+			<Slider
+				speed={500}
+				slidesToScroll={1}
+				slidesToShow={1}
+				autoplaySpeed={5000}
+				easing='in-out'
+				dots
+				infinite
+				autoplay
+			>
+				{testimonials.map(({ name, review }, i) => (
+					<div key={i.toString()}>
+						By {name}
+						<br />
+						{review}
+					</div>
+				))}
+			</Slider>
+		</Container>
 	</Fragment>
 );
 
