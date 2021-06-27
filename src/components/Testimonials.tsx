2154d00ef465a2c472679555b12619df627e958f
Riday ♡ <ridayshah2001@gmail.com>
2021-06-27 11:36:41 +0530

    🎨 Refactor: split into multiple files
    - Split static data into another file for cleaner code structure.

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
 import Slider from 'react-slick';
 import styled from 'styled-components';
+import testimonials from '../util/testimonials';
 
 const Container = styled.div`
 	margin-bottom: 10vh;
@@ -11,76 +12,6 @@ const Container = styled.div`
 		cursor: grabbing;
 	}
 `;
-
-interface Testimonial {
-	name: string;
-	review: string;
-}
-
-// cSpell: disable
-const testimonials: Array<Testimonial> = [
-	{
-		name: 'Test',
-		review:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
-			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
-			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
-			' Ut vulputate sapien sed rhoncus bibendum.' +
-			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
-			' Vivamus quis nulla sem.' +
-			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
-	},
-	{
-		name: 'Test',
-		review:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
-			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
-			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
-			' Ut vulputate sapien sed rhoncus bibendum.' +
-			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
-			' Vivamus quis nulla sem.' +
-			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
-	},
-	{
-		name: 'Test',
-		review:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
-			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
-			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
-			' Ut vulputate sapien sed rhoncus bibendum.' +
-			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
-			' Vivamus quis nulla sem.' +
-			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
-	},
-	{
-		name: 'Test',
-		review:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
-			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
-			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
-			' Ut vulputate sapien sed rhoncus bibendum.' +
-			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
-			' Vivamus quis nulla sem.' +
-			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
-	},
-	{
-		name: 'Test',
-		review:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
-			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
-			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
-			' Ut vulputate sapien sed rhoncus bibendum.' +
-			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
-			' Vivamus quis nulla sem.' +
-			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
-	}
-];
-// cSpell: enable
 
 const Testimonials = () => (
 	<Fragment>
---
+++ src/util/testimonials.ts
@@ -0,0 +1,71 @@
+interface Testimonial {
+	name: string;
+	review: string;
+}
+
+// cSpell: disable
+const testimonials: Array<Testimonial> = [
+	{
+		name: 'Test',
+		review:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
+			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
+			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
+			' Ut vulputate sapien sed rhoncus bibendum.' +
+			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
+			' Vivamus quis nulla sem.' +
+			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
+	},
+	{
+		name: 'Test',
+		review:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
+			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
+			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
+			' Ut vulputate sapien sed rhoncus bibendum.' +
+			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
+			' Vivamus quis nulla sem.' +
+			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
+	},
+	{
+		name: 'Test',
+		review:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
+			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
+			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
+			' Ut vulputate sapien sed rhoncus bibendum.' +
+			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
+			' Vivamus quis nulla sem.' +
+			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
+	},
+	{
+		name: 'Test',
+		review:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
+			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
+			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
+			' Ut vulputate sapien sed rhoncus bibendum.' +
+			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
+			' Vivamus quis nulla sem.' +
+			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
+	},
+	{
+		name: 'Test',
+		review:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
+			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
+			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
+			' Ut vulputate sapien sed rhoncus bibendum.' +
+			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
+			' Vivamus quis nulla sem.' +
+			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
+	}
+];
+// cSpell: enable
+
+export default testimonials;
