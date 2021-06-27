430e2015a0fcff71dc59751d00b248ed6d1f3e2a
Riday ♡ <ridayshah2001@gmail.com>
2021-06-27 14:00:08 +0530

    🍱 Chore: added icons

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,8 +1,11 @@
 export { default as ArrowDownIcon } from './ArrowDown';
 export { default as BookingIcon } from './Booking';
 export { default as ContactIcon } from './Contact';
+export { default as EmailIcon } from './Email';
 export { default as GrowthIcon } from './Growth';
 export { default as LearningIcon } from './Learning';
-export { default as SelectIcon } from './Select';
-export { default as StepUpIcon } from './StepUp';
 export { default as RecordingIcon } from './Recording';
+export { default as SelectIcon } from './Select';
+export { default as SheetIcon } from './Sheet';
+export { default as StepUpIcon } from './StepUp';
+export { default as WhatsAppIcon } from './WhatsApp';
