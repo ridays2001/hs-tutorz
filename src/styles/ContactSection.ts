6c56ae905c4cbf4ff9e0b71e515aa371e5a0b2e9
Riday ♡ <ridayshah2001@gmail.com>
2021-07-02 09:42:44 +0530

    💄 Style: updated styles for contact section

--- src/styles/ContactSection.ts
+++ src/styles/ContactSection.ts
@@ -1,13 +1,31 @@
 import styled from 'styled-components';
-import { colors, shadows } from '../theme';
+import { colors, devices, shadows } from '../theme';
 
 const ContactSection = styled.section`
 	section {
 		border-radius: 21px;
 		box-shadow: ${shadows.layer1};
 		padding: 2em;
-		background-color: ${colors.primary};
-		color: #ffffff;
+	}
+
+	#num {
+		margin-top: 2em;
+	}
+
+	.highlight {
+		display: inline-block;
+		margin-left: 5px;
+		margin-right: 5px;
+		padding-left: 5px;
+		padding-right: 5px;
+		font-family: 'Roboto Slab';
+		background-color: ${colors.highlightMid};
+	}
+
+	${devices.md} {
+		#num {
+			margin-top: 0;
+		}
 	}
 `;
 
