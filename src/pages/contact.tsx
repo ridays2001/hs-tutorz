2b728a7185c0d24e733a39f043fc75b8afaee573
Riday ♡ <ridayshah2001@gmail.com>
2021-07-02 09:43:14 +0530

    💬 Chore: replaced static values with env vars

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -8,29 +8,36 @@ const ContactPage = () => (
 	<Layout page='contact'>
 		<SEO title='Contact Us' description='The contact page.' />
 		<ContactSection className='col-md-9 mx-auto text-center'>
-			<h1 className='text-center my-5'>Contact Us</h1>
+			<h1 className='text-center my-5'>Get In Touch</h1>
 			<section className='row col-md-9 mx-auto'>
 				<div className='col-md-6'>
-					Email Us on:
+					Email Us on <span className='highlight'>{process.env.GATSBY_EMAIL}</span>
+					<br />
+					or click on the button below:
 					<br />
 					<br />
-					<a href='mailto:hstutorialz@gmail.com' className='btn btn-secondary'>
-						<EmailIcon /> &ensp; hstutorialz@gmail.com
+					<a href={`mailto:${process.env.GATSBY_EMAIL}`} className='btn btn-primary'>
+						<EmailIcon /> &ensp; Email
 					</a>
 				</div>
-				<div className='col-md-6'>
-					Call or WhatsApp Us on:
+				<div className='col-md-6' id='num'>
+					Call or WhatsApp Us on <span className='highlight'>+91 {process.env.GATSBY_PHONE}</span>
+					<br />
+					or click on the button below:
 					<br />
 					<br />
-					<a href='https://wa.me/919820093845' className='btn btn-secondary'>
-						<WhatsAppIcon /> &ensp; +91 98200 93845
+					<a
+						href={`https://wa.me/91${process.env.GATSBY_PHONE?.replace(/\s/g, '')}`}
+						className='btn btn-primary'
+					>
+						<WhatsAppIcon /> &ensp; WhatsApp
 					</a>
 				</div>
 				<div className='col-12 my-5'>
-					Or fill the following contact form to hear back from us within 24 hours.
+					Or you can fill the following contact form and we will get back to you within 24 hours.
 					<br />
 					<br />
-					<a href={process.env.GATSBY_CONTACT_FORM} className='btn btn-secondary'>
+					<a href={process.env.GATSBY_CONTACT_FORM} className='btn btn-primary'>
 						<SheetIcon /> &ensp; Contact Form
 					</a>
 				</div>
