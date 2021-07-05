000ec38f1405b3316670cbaec41029c066ac9009
Riday ♡ <ridayshah2001@gmail.com>
2021-07-05 10:28:55 +0530

    💬 Chore: updated default description

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -6,15 +6,17 @@ interface Props {
 	description: string;
 }
 
+const desc = 'Online tutorials for high school and undergraduate arts and commerce.';
+
 const SEO = ({ title, description }: Props) => (
 	<Helmet>
 		<title>{title} | HS Tutorz</title>
-		<meta name='description' content={description || 'A Tutorials business website.'} />
+		<meta name='description' content={description || desc} />
 		<meta name='subject' content='HS Tutorz' />
 		<meta name='rating' content='General' />
 
 		<meta property='og:title' content='HS Tutorz' />
-		<meta property='og:description' content='A Tutorials business website.' />
+		<meta property='og:description' content={desc} />
 		<meta property='og:locale' content='en_IN' />
 		<meta property='article:author' content='Riday Shah' />
 	</Helmet>
