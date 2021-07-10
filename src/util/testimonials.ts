d47542b0903296960ea38b5727a2c598dddf1986
Riday ♡ <ridayshah2001@gmail.com>
2021-07-10 15:48:49 +0530

    💬 Chore: updated testimonails

--- src/util/testimonials.ts
+++ src/util/testimonials.ts
@@ -6,64 +6,34 @@ interface Testimonial {
 // cSpell: disable
 const testimonials: Array<Testimonial> = [
 	{
-		name: 'Test',
+		name: 'Sia (Student)',
 		review:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
-			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
-			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
-			' Ut vulputate sapien sed rhoncus bibendum.' +
-			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
-			' Vivamus quis nulla sem.' +
-			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
+			'Himaani Ma’am is the first teacher that I’ve met who actually seems to care about me.' +
+			' She is always ready to clear my doubts, even when we are doing a completely different topic.' +
+			' She also makes sure that I understand everything properly. Also, even if I ask about something not' +
+			' directly related to my studies, she readily helps me and gives me advice.' +
+			' I’m really happy that I joined and would definitely recommend people to join.'
 	},
 	{
-		name: 'Test',
+		name: 'Hunny (Parent)',
 		review:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
-			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
-			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
-			' Ut vulputate sapien sed rhoncus bibendum.' +
-			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
-			' Vivamus quis nulla sem.' +
-			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
+			'I am really happy with the teaching methods and techniques by which she makes learning fun!' +
+			' I would wholeheartedly recommend tutoring with Ms. Himaani.'
 	},
 	{
-		name: 'Test',
+		name: 'Nisha (Parent)',
 		review:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
-			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
-			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
-			' Ut vulputate sapien sed rhoncus bibendum.' +
-			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
-			' Vivamus quis nulla sem.' +
-			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
+			'The teacher taught Aarna (my daughter) academics very well and explained her concepts nicely.' +
+			' She even explained her life challenges very nicely how to deal with them and kept fun sessions' +
+			' which were quite knowledgeable.'
 	},
 	{
-		name: 'Test',
+		name: 'Prachi (Student)',
 		review:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
-			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
-			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
-			' Ut vulputate sapien sed rhoncus bibendum.' +
-			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
-			' Vivamus quis nulla sem.' +
-			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
-	},
-	{
-		name: 'Test',
-		review:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-			' In vitae placerat lorem. Pellentesque velit elit, facilisis eu magna et, maximus vehicula turpis.' +
-			' Aliquam fermentum justo quis urna fermentum auctor. Donec scelerisque quis lectus euismod condimentum.' +
-			' Maecenas neque odio, interdum ac efficitur bibendum, facilisis quis est.' +
-			' Ut vulputate sapien sed rhoncus bibendum.' +
-			' Etiam luctus, nisi a laoreet dictum, ex odio egestas dui, eu vestibulum metus ipsum molestie quam.' +
-			' Vivamus quis nulla sem.' +
-			' Nullam feugiat, lectus ut sollicitudin auctor, nibh eros feugiat urna, non tempor nisl tortor id neque.'
+			'Ms. Himaani is an extraordinary teacher! Her teaching is amazing and her teaching technique' +
+			' is different from others. I want to thank her for giving us something to carry home each day, something to' +
+			' help us grow with every lesson. She always kept saying ‘You can do better’ and those words made me work harder.' +
+			' Today I’ve become who I am because she kept pushing. I really like her and would readily recommend her.'
 	}
 ];
 // cSpell: enable
